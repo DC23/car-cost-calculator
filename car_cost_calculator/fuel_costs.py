d26f4c54ec56da5b8627a26283831870536c5346
DC23 <jugglindan@gmail.com>
2018-08-31 22:18:07 +0800

    fuel costs

---
+++ car_cost_calculator/fuel_costs.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+'fuel cost functions'
+# pylint: disable=R0903
+from .compound_interest import compound_interest
+
+
+def fuel_used(distance_km: float, litres_per_100km: float):
+    '''
+    Calculates the amount of fuel used for a given number of kilometers and fuel efficiency.
+
+    Args:
+        distance_km (float): Distance driven in km.
+        litres_per_100km (float): Fuel efficiency in litres per 100km.
+
+    Returns:
+        float: quantity of fuel used.
+    '''
+    return distance_km * litres_per_100km / 100.0
+
+
+def yearly_fuel_cost(km_per_year, litres_per_100km: float, years: int,
+                     inflation: float, initial_fuel_price: float):
+    '''
+    Calculates the yearly indexed fuel cost based on an inflation rate,
+    initial fuel cost, and number of km driven per year.
+
+    Args:
+        km_per_year: Distance in kilometres driven per year.
+        litres_per_100km (float): Fuel efficiency in litres per 100km.
+        years (int): Number of years to calculate.
+        inflation (float): Inflation rate of fuel price.
+        initial_fuel_price (float): Initial fuel price in dollars per litre.
+
+    Returns:
+        numpy.array: Yearly total fuel cost.
+    '''
+    assert inflation >= 0
+    assert inflation <= 1.0
+
+    fuel_used_per_year = fuel_used(km_per_year, litres_per_100km)
+    indexed_fuel_cost_per_litre = compound_interest(
+        principal=initial_fuel_price,
+        annual_rate=inflation,
+        years=years,
+        compounds_per_year=1)
+    return indexed_fuel_cost_per_litre * fuel_used_per_year
---
+++ car_cost_calculator/tests/test_fuel_costs.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=C0103, C0111
+
+from numpy.testing import assert_allclose
+from car_cost_calculator.fuel_costs import fuel_used, yearly_fuel_cost
+
+
+def test_fuel_used_100km():
+    assert fuel_used(100, 8) == 8.0
+
+
+def test_fuel_used_1000km():
+    assert fuel_used(1000.0, 12.2) == 122.0
+
+
+def test_fuel_used_15000km():
+    assert fuel_used(15000, 15.8) == 2370.0
+
+
+def test_fuel_cost_over_10years():
+    expected = [
+        1890.0, 1929.69, 1970.214, 2011.588, 2053.831, 2096.962, 2140.998,
+        2185.959, 2231.864, 2278.733
+    ]
+    actual = yearly_fuel_cost(
+        km_per_year=15000,
+        litres_per_100km=8.4,
+        years=10,
+        inflation=2.1 / 100.0,
+        initial_fuel_price=1.5)
+    assert_allclose(actual, expected, atol=0.001)
