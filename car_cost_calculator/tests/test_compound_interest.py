7bf4ab0cedbdbcc7b102db8409fb3e51cd1bd6cb
DC23 <jugglindan@gmail.com>
2018-08-31 15:53:27 +0800

    changing - to _

--- car_cost_calculator/tests/test_compound_interest.py
+++ car_cost_calculator/tests/test_compound_interest.py
@@ -1,13 +1,12 @@
 'Compound interest tests'
-from .. compound_interest import compound_interest
-
 import numpy as np
+import car_cost_calculator as ccc
 
 def test_compound_interest_num_results():
-    assert len(compound_interest(8000, 0.073, 10, 1)) == 10
+    assert len(ccc.compound_interest(8000, 0.073, 10, 1)) == 10
 
 def test_compound_interest():
-    actual = compound_interest(8000, 0.073, 5, 1)
+    actual = ccc.compound_interest(8000, 0.073, 5, 1)
     expected = [8000.0, 8584.0, 9210.632, 9883.008, 10604.468]
     np.testing.assert_allclose(actual, expected, verbose=True)
     
