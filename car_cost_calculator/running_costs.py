15e97a19e078b8785b62f74755e9e8a714e6e21b
DC23 <jugglindan@gmail.com>
2018-09-01 22:29:40 +0800

    started adding running costs class

---
+++ car_cost_calculator/running_costs.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'Running costs class'
+
+# km_per_year
+# years
+# fuel costs
+# tyres - cost per tyre and replacement interval (km)
+# depreciation
+# service_cost = 400,
+# service_interval_km = 10000,
+# service_interval_years = 1.0,
--- car_cost_calculator/standing_costs.py
+++ car_cost_calculator/standing_costs.py
@@ -4,6 +4,7 @@
 from .compound_interest import compound_interest
 
 
+# TODO: add wash/detailing
 class StandingCosts():
     '''Represents static yearly costs which are not dependent on distance travelled'''
 
@@ -15,9 +16,7 @@ class StandingCosts():
                  roadside_assist_per_year=200):
         '''Initialise the standing costs object'''
         self.insurance_per_year = compound_interest(
-            principal=insurance_per_year,
-            annual_rate=inflation,
-            years=years)
+            principal=insurance_per_year, annual_rate=inflation, years=years)
         self.registration_per_year = compound_interest(
             principal=registration_per_year,
             annual_rate=inflation,
