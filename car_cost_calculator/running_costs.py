0037f4c95c77b32b87d5775d31f36002cda10e95
Daniel Collins <daniel.collins@csiro.au>
2018-09-03 16:40:36 +0800

    tyre replacement cost calculations

--- car_cost_calculator/running_costs.py
+++ car_cost_calculator/running_costs.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 'Running costs class'
 
+import numpy as np
+from .compound_interest import compound_interest
 from .depreciation import FlatRate, calculate
 from .fuel_costs import yearly_fuel_cost
 
@@ -9,15 +11,25 @@ class RunningCosts():
     '''Represents yearly running costs that depend on distance travelled'''
 
     def __init__(self,
-                 initial_vehicle_value: float,
+                 initial_vehicle_value: float = 40000,
                  initial_vehicle_age: int = 0,
                  depreciation_rate: callable = FlatRate(),
                  years: int = 10,
                  km_per_year: float = 15000.0,
                  litres_per_100km: float = 10.0,
                  inflation: float = 0.02,
-                 initial_fuel_price: float = 1.50):
-        # TODO tyres - cost per tyre and replacement interval (km)
+                 initial_fuel_price: float = 1.50,
+                 tyre_replacement_interval=15000,
+                 initial_cost_per_tyre=300):
+
+        self.years = years
+        self.km_per_year = km_per_year
+        self.litres_per_100km = litres_per_100km
+        self.inflation = inflation
+
+        self.cumulative_distance = np.linspace(
+            km_per_year, km_per_year * years, num=years)
+
         # TODO service_cost = 400,
         # TODO service_interval_km = 10000,
         # TODO service_interval_years = 1.0,
@@ -34,3 +46,34 @@ class RunningCosts():
             years=years,
             inflation=inflation,
             initial_fuel_price=initial_fuel_price)
+
+        self.indexed_cost_per_tyre = compound_interest(
+            principal=initial_cost_per_tyre,
+            annual_rate=inflation,
+            years=years)
+
+        self.tyre_cost = self.__calc_tyre_cost(tyre_replacement_interval)
+
+    def __calc_tyre_cost(self, tyre_replacement_interval):
+        '''Calculate tyre replacement costs'''
+        # The cumulative km milestones for tyre replacement
+        replacement_milestones = np.arange(tyre_replacement_interval,
+                                           self.cumulative_distance[-1] + 1,
+                                           tyre_replacement_interval)
+
+        # Indicies of the year in which each replacement will fall, based on the cumulative yearly distance
+        replacement_indicies = np.digitize(
+            x=replacement_milestones,
+            bins=self.cumulative_distance,
+            right=True)
+
+        # unique year indicices and the count of replacement events in that year
+        unique_replacement_years, replacement_counts = np.unique(
+            replacement_indicies, return_counts=True)
+
+        # finally, the total cost of tyre replacements for each year,
+        # assuming that 4 tyres are replaced (spares are typically longer-lasting)
+        yearly_tyre_cost = np.zeros(self.years)
+        for count_idx, year_idx in enumerate(unique_replacement_years):
+            yearly_tyre_cost[year_idx] = replacement_counts[count_idx] * self.indexed_cost_per_tyre[year_idx] * 4
+        return yearly_tyre_cost
--- car_cost_calculator/tests/test_running_costs.py
+++ car_cost_calculator/tests/test_running_costs.py
@@ -2,12 +2,56 @@
 # pylint: disable=C0103, C0111
 
 # import numpy as np
-# from numpy.testing import assert_allclose
-# from car_cost_calculator.running_costs import RunningCosts
+from numpy.testing import assert_allclose
+from car_cost_calculator.depreciation import FlatRate
+from car_cost_calculator.running_costs import RunningCosts
 
 
 def test_depreciation_on_10K_over_5_years_at_11percent():
-    # actual, _ = calculate(10000, years=5, initial_age=0, rate=FlatRate(0.11))
-    # expected = [10000.000, 8900.000, 7921.000, 7049.690, 6274.224]
-    # assert_allclose(actual, expected, atol=0.001)
-    pass
+    actual = RunningCosts(
+        initial_vehicle_value=10000,
+        initial_vehicle_age=0,
+        depreciation_rate=FlatRate(0.11),
+        years=5)
+    expected = [10000.000, 8900.000, 7921.000, 7049.690, 6274.224]
+    assert_allclose(actual.depreciated_value, expected, atol=0.001)
+
+def test_fuel_cost():
+    expected = [
+        1890.0, 1929.69, 1970.214, 2011.588, 2053.831, 2096.962, 2140.998,
+        2185.959, 2231.864, 2278.733
+    ]
+    actual = RunningCosts(
+        km_per_year=15000,
+        litres_per_100km=8.4,
+        years=10,
+        inflation=2.1 / 100.0,
+        initial_fuel_price=1.5)
+    assert_allclose(actual.fuel_cost, expected, atol=0.001)
+
+def test_cumulative_distance():
+    actual = RunningCosts(years=5, km_per_year=1000)
+    expected = [1000, 2000, 3000, 4000, 5000]
+    assert_allclose(actual.cumulative_distance, expected)
+
+def test_tyre_replacement_schedule():
+    actual = RunningCosts(
+        years=5,
+        inflation=0.0,
+        km_per_year=10000,
+        tyre_replacement_interval=16000,
+        initial_cost_per_tyre=1)
+    # One replacement event in years 2, 4 & 5
+    expected = [0, 4, 0, 4, 4]
+    assert_allclose(actual.tyre_cost, expected)
+
+def test_tyres_more_than_once_per_year():
+    actual = RunningCosts(
+        years=3,
+        inflation=0.01,
+        km_per_year=10000,
+        tyre_replacement_interval=3000,
+        initial_cost_per_tyre=1)
+    # 3 replacements in first year, 3 in second, 4 in third
+    expected = [12.0, 12.12, 16.3216]
+    assert_allclose(actual.tyre_cost, expected)
