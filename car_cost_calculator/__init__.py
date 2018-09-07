e2f1b76750957e0f444db6946e23d24bdaffdcd5
Daniel Collins <daniel.collins@csiro.au>
2018-09-07 15:33:55 +0800

    updating package imports to expose some functions at the package level

--- car_cost_calculator/__init__.py
+++ car_cost_calculator/__init__.py
@@ -7,3 +7,9 @@
 __author__ = 'DC23'
 __email__ = 'jugglindan@gmail.com'
 __version__ = '0.5.0'
+
+from .car_costs import CarCosts
+from .compound_interest import compound_interest
+from .depreciation import FlatRate, TwoStageRate
+from .running_costs import RunningCosts
+from .standing_costs import StandingCosts
